74c77f9fd3c18bee9dd27573b4b8b4648b465c01
Yankes <Yankes@users.noreply.github.com>
2024-09-14 00:03:17 +0200

    y-script: Add time passed from start of game

--- src/Savegame/SavedGame.cpp
+++ src/Savegame/SavedGame.cpp
@@ -3565,6 +3565,8 @@ void SavedGame::ScriptRegister(ScriptParserBase* parser)
 	sgg.add<&getRandomScript>("getRandomState");
 
 	sgg.add<&difficultyLevelScript>("difficultyLevel", "Get difficulty level");
+	sgg.add<&SavedGame::getMonthsPassed>("getMonthsPassed", "Number of months passed from start");
+	sgg.add<&SavedGame::getDaysPassed>("getDaysPassed", "Number of days passed from start");
 
 	sgg.add<&isResearchedScript>("isResearched");
 
